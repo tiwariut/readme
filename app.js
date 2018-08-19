6f3f8c3c4609da63d9dab4d202f83ef2bb700aad
Utkarsh <tiwariutkarsh007@gmail.com>
2018-08-19 09:31:59 +0000

    Add initial routes

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require("express"),
     mongoose = require("mongoose"),
     app = express();
 
-
+//APP CONFIG
 app.set("view engine", "ejs");
-mongoose.connect("mongodb://localhost:27017/read_me", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/read_me", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT, process.env.IP, function(){
+//ROOT ROUTE
+app.get("/", function(req, res) {
+    res.redirect("/posts");
+});
+
+//INDEX ROUTE
+app.get("/posts", function(req, res) {
+    res.render("index");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running!");
 });
