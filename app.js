27e7cffac94e6c8deb3bea79fb0623dae94e0fa8
Utkarsh <tiwariutkarsh007@gmail.com>
2018-08-19 19:41:49 +0000

    Add new and create routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    expressSanitizer = require("express-sanitizer"),
     Post = require("./models/post"),
     seedDB = require("./seeds"),
     app = express();
@@ -9,6 +10,7 @@ var express = require("express"),
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/read_me", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
 app.use(express.static(__dirname + "/public"));
 
 seedDB();
@@ -25,6 +27,23 @@ app.get("/posts", function(req, res) {
             console.log(err);
         } else{
             res.render("index", {posts: allPosts});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/posts/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/posts", function(req, res){
+    req.body.post.body = req.sanitize(req.body.post.body);
+    Post.create(req.body.post, function(err, newPost){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts");
         }
     });
 });
