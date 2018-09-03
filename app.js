c7fdabdff3d6e0cda62a860c1392d5633b77334c
Utkarsh <tiwariutkarsh007@gmail.com>
2018-09-03 18:26:00 +0000

    Revert back

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var express = require("express"),
     Comment = require("./models/comment"),
     seedDB = require("./seeds"),
     app = express();
-    
-var postsRoutes = require("./routes/posts"),
-    commentsRoutes = require("./routes/comments"),
-    indexRoutes = require("./routes/index");
 
 //APP CONFIG
 app.set("view engine", "ejs");
@@ -22,9 +18,161 @@ app.use(methodOverride("_method"));
 
 seedDB();
 
-app.use(postsRoutes);
-app.use(commentsRoutes);
-app.use(indexRoutes);
+//ROOT ROUTE
+app.get("/", function(req, res) {
+    res.redirect("/posts");
+});
+
+//============
+//POSTS ROUTES
+//============
+
+//INDEX ROUTE
+app.get("/posts", function(req, res) {
+    Post.find({}, function(err, allPosts){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {posts: allPosts});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/posts/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/posts", function(req, res){
+    req.body.post.body = req.sanitize(req.body.post.body);
+    Post.create(req.body.post, function(err, newPost){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/posts/:id", function(req, res) {
+    Post.findById(req.params.id).populate("comments").exec(function(err, foundPost){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show", {post: foundPost});
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/posts/:id/edit", function(req, res) {
+    Post.findById(req.params.id, function(err, foundPost){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("edit", {post: foundPost});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/posts/:id", function(req, res){
+    req.body.post.body = req.sanitize(req.body.post.body);
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
+
+//DESTROY ROUTE
+app.delete("/posts/:id", function(req, res){
+    Post.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts");
+        }
+    });
+});
+
+//===============
+//COMMENTS ROUTES
+//===============
+
+
+//NEW ROUTE
+app.get("/posts/:id/comments/new", function(req, res){
+    Post.findById(req.params.id, function(err, foundPost) {
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {post: foundPost});
+        }
+    });
+});
+
+//CREATE ROUTE
+app.post("/posts/:id/comments", function(req, res){
+    Post.findById(req.params.id, function(err, foundPost){
+        if(err){
+            console.log(err);
+        } else{
+            Comment.create(req.body.comment, function(err, newComment){
+                if(err){
+                    console.log(err);
+                } else{
+                    foundPost.comments.push(newComment);
+                    foundPost.save();
+                    res.redirect("/posts/" + req.params.id);
+                }
+            });
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/posts/:id/comments/:comment_id/edit", function(req, res) {
+    Post.findById(req.params.id, function(err, foundPost) {
+        if(err){
+            console.log(err);
+        } else{
+            Comment.findById(req.params.comment_id, function(err, foundComment) {
+                if(err){
+                    console.log(err);
+                } else{
+                    res.render("comments/edit", {post: foundPost, comment: foundComment});
+                }
+            });
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/posts/:id/comments/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
+
+//DESTROY ROUTE
+app.delete("/posts/:id/comments/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is running!");
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express"),
-    router = express.Router();
-
-//ROOT ROUTE
-router.get("/", function(req, res) {
-    res.redirect("/posts");
-});
-
-module.exports = router;
