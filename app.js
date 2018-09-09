828dbfe98ad4927ebd66e405289eb44ae0181712
Utkarsh <tiwariutkarsh007@gmail.com>
2018-09-09 19:42:46 +0000

    Refactor post routes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var express = require("express"),
 //REQUIRING ROUTES
 
 var indexRoutes = require("./routes/index"),
+    postRoutes = require("./routes/posts"),
     userRoutes = require("./routes/users");
 
 //APP CONFIG
@@ -53,100 +54,8 @@ app.use(function(req, res, next) {
 //seedDB();
 
 app.use("/", indexRoutes);
+app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
-
-//============
-//POSTS ROUTES
-//============
-
-//INDEX ROUTE
-app.get("/posts", function(req, res) {
-    Post.find({}, function(err, allPosts){
-        if(err){
-            console.log(err);
-        } else{
-            res.render("posts/index", {posts: allPosts});
-        }
-    });
-});
-
-//NEW ROUTE
-app.get("/posts/new", middleware.isLoggedIn, function(req, res){
-    res.render("posts/new");
-});
-
-//CREATE ROUTE
-app.post("/posts", middleware.isLoggedIn, function(req, res){
-    req.body.post.body = req.sanitize(req.body.post.body);
-    Post.create(req.body.post, function(err, newPost){
-        if(err){
-            req.flash("error", "Not able to create your post.");
-            res.redirect("back");
-            console.log(err);
-        } else{
-            newPost.author.id = req.user._id;
-            newPost.author.username = req.user.username;
-            newPost.save();
-            req.flash("success", "Added your post.");
-            res.redirect("/posts");
-        }
-    });
-});
-
-//SHOW ROUTE
-app.get("/posts/:id", function(req, res) {
-    Post.findById(req.params.id).populate("comments").exec(function(err, foundPost){
-        if(err){
-            req.flash("error", "Post not found.");
-            res.redirect("back");
-            console.log(err);
-        } else{
-            res.render("posts/show", {post: foundPost});
-        }
-    });
-});
-
-//EDIT ROUTE
-app.get("/posts/:id/edit", middleware.checkPostOwnership, function(req, res) {
-    Post.findById(req.params.id, function(err, foundPost){
-        if(err){
-            req.flash("error", "Post not found.");
-            res.redirect("back");
-            console.log(err);
-        } else{
-            res.render("posts/edit", {post: foundPost});
-        }
-    });
-});
-
-//UPDATE ROUTE
-app.put("/posts/:id", middleware.checkPostOwnership, function(req, res){
-    req.body.post.body = req.sanitize(req.body.post.body);
-    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
-        if(err){
-            req.flash("error", "Post not found.");
-            res.redirect("back");
-            console.log(err);
-        } else{
-            req.flash("success", "Updated your post.");
-            res.redirect("/posts/" + req.params.id);
-        }
-    });
-});
-
-//DESTROY ROUTE
-app.delete("/posts/:id", middleware.checkPostOwnership, function(req, res){
-    Post.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            req.flash("error", "Post not found.");
-            res.redirect("back");
-            console.log(err);
-        } else{
-            req.flash("success", "Deleted your post.");
-            res.redirect("/posts");
-        }
-    });
-});
 
 //===============
 //COMMENTS ROUTES
